1823d55e60a6f8a905c2cd929354e4ebde60db5c
Hoang Tuan Anh <anhhtbk@gmail.com>
2018-03-17 00:31:38 +0700

    Initial Commit

---
+++ KMLTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KMLTest
+//
+//  Created by Hoang Tuan Anh on 3/17/18.
+//  Copyright © 2018 Hoang Tuan Anh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KMLTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KMLTest
+//
+//  Created by Hoang Tuan Anh on 3/17/18.
+//  Copyright © 2018 Hoang Tuan Anh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
