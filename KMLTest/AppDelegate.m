7825a3394b0822078c99dba08a3cd2f6512ae16c
Hoang Tuan Anh <anhhtbk@gmail.com>
2018-03-17 01:21:08 +0700

    add source

--- KMLTest/AppDelegate.m
+++ KMLTest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import GoogleMaps;
 
 @interface AppDelegate ()
 
@@ -16,7 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
+
+  [GMSServices provideAPIKey:@"AIzaSyDbXcoAiKieZ9jhYJ52pBUvymVJwotu7I0"];
   return YES;
 }
 
--- KMLTest/ViewController.m
+++ KMLTest/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <GMUKMLParser.h>
+#import <GMUGeometryRenderer.h>
+#import <GMUPlacemark.h>
+#import <GMULineString.h>
+
+@import GoogleMaps;
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet GMSMapView *mapView;
 
 @end
 
@@ -16,7 +24,22 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+
+  NSString *path = [[NSBundle mainBundle] pathForResource:@"Untitled Path" ofType:@"kml"];
+  NSURL *url = [NSURL fileURLWithPath:path];
+  GMUKMLParser *parser = [[GMUKMLParser alloc] initWithURL:url];
+  [parser parse];
+  GMUGeometryRenderer *renderer = [[GMUGeometryRenderer alloc] initWithMap:_mapView
+                                                                geometries:parser.placemarks
+                                                                    styles:parser.styles];
+
+  GMULineString *line = parser.placemarks.firstObject.geometry;
+  for (int i = 0; i < line.path.count; i ++) {
+    CLLocationCoordinate2D coorDinate = [line.path coordinateAtIndex:i];
+    NSLog(@"%f, %f", coorDinate.latitude, coorDinate.longitude);
+  }
+  [renderer render];
+  _mapView.camera = [GMSCameraPosition cameraWithLatitude: 37.421552 longitude:-122.084271 zoom:16];
 }
 
 
